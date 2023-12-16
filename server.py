c9f932ff335c7c13951c5e150954c034ba5a210a
Aditi-Negii <aditi.negi7304@gmail.com>
2023-12-16 20:13:03 +0530

    Final Commit

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__, static_folder='client/build', static_url_path='')
